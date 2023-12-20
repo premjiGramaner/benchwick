2bbe7071b82a577c731714bc62de405ebb31890c
harshan99coder <131944074+harshan99coder@users.noreply.github.com>
2023-12-20 18:27:37 +0530

    DVE:Add search pagination sort & functionality

--- src/pages/Viewhistory/Viewhistory.tsx
+++ src/pages/Viewhistory/Viewhistory.tsx
@@ -20,7 +20,6 @@ import { IReducerState } from '@Interface/StoreInterface'
 import { getEnvisionVariants } from 'src/reducers/getEnvisionVariantsReducer'
 const Viewhistory: React.FC<IDefaultPageProps> = props => {
   const [searchInput, setSearchInput] = useState<string>('')
-  const [handlePageCount, setHandlePageCount] = useState<number>(10)
   const [selectedPage, setSelectedPage] = useState<number>(1)
   const [variationmodal, setvariationModal] = useState(false)
   const { data = [], isError } = useSelector(
@@ -29,10 +28,75 @@ const Viewhistory: React.FC<IDefaultPageProps> = props => {
   const { variantData = {} } = useSelector(
     (state: IReducerState) => state.getEnvisionVariantsReducer
   )
+  const [handlePageCount, setHandlePageCount] = useState<number>(10)
 
   // useEffect(() => {
   //   props.dispatch(actions.fetchViewhistoryRequested(`page=${selectedPage}`))
   // }, [selectedPage])
+  const [tableData, setTableData] = useState(data || [])
+  const [sortOrder, setSortOrder] = useState<string>('asc') // or 'desc'
+  const [sortBy, setSortBy] = useState<string>('name')
+  useEffect(() => {
+    setTableData(data)
+  }, [data])
+  const sortHandle = field => {
+    const isDateOrTime = ['created_date', 'created_time'].includes(field)
+    const isNumeric = field === 'variants'
+
+    let sortedData = [...tableData]
+    sortedData = sortedData.sort((a, b) => {
+      if (isDateOrTime) {
+        const dateA = new Date(a[field])
+        const dateB = new Date(b[field])
+        const compareValue = dateA > dateB ? 1 : -1
+        return sortOrder === 'asc' ? compareValue : -compareValue
+      }
+
+      if (isNumeric) {
+        const compareValue = a[field] - b[field]
+        return sortOrder === 'asc' ? compareValue : -compareValue
+      }
+
+      const compareValue = a[field].localeCompare(b[field])
+      return sortOrder === 'asc' ? compareValue : -compareValue
+    })
+
+    setTableData(sortedData)
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')
+    setSortBy(field)
+  }
+
+  const filteredAndSortedData = tableData
+    .filter(item =>
+      Object.values(item).some(
+        value =>
+          typeof value === 'string' &&
+          value.toLowerCase().includes(searchInput.toLowerCase())
+      )
+    )
+    .sort((a, b) => {
+      if (sortBy === 'date' || sortBy === 'time' || sortBy === 'variants') {
+        const isDateOrTime = ['created_date', 'created_time'].includes(sortBy)
+        const isNumeric = sortBy === 'variants'
+
+        if (isDateOrTime) {
+          const dateA = new Date(a[sortBy])
+          const dateB = new Date(b[sortBy])
+          const compareValue = dateA > dateB ? 1 : -1
+          return sortOrder === 'asc' ? compareValue : -compareValue
+        }
+
+        if (isNumeric) {
+          const compareValue = a[sortBy] - b[sortBy]
+          return sortOrder === 'asc' ? compareValue : -compareValue
+        }
+      }
+      const compareValue = a[sortBy].localeCompare(b[sortBy])
+      return sortOrder === 'asc' ? compareValue : -compareValue
+    })
+  const startIndex = (selectedPage - 1) * handlePageCount
+  const endIndex = startIndex + handlePageCount
+  const currentDataPage = filteredAndSortedData.slice(startIndex, endIndex)
 
   const handleLogout = () => {
     // Do the logout API call and get the success result
@@ -42,16 +106,29 @@ const Viewhistory: React.FC<IDefaultPageProps> = props => {
   const handleBackToDashBoard = () => {
     props.navigate(URLS.DASHBOARD)
   }
+  const totalItems = filteredAndSortedData.length
+  const totalPages = Math.ceil(totalItems / handlePageCount)
 
-  const paginationRange = Array?.from({ length: 10 }, (_, i) => i + 1) || []
+  const paginationRange =
+    Array.from({ length: totalPages }, (_, i) => i + 1) || []
 
-  const onNext = () => setSelectedPage(selectedPage + 1)
-  const onPrevious = () => setSelectedPage(selectedPage - 1)
-  const onFirst = () => setSelectedPage(1)
-  const onLast = () => setSelectedPage(paginationRange.length)
-
-  const sortHandle = () => {
-    console.log('sort is clicked')
+  const onFirst = () => {
+    setSelectedPage(1)
+  }
+  const onPrevious = () => {
+    if (selectedPage > 1) {
+      setSelectedPage(selectedPage - 1)
+    }
+  }
+  const onNext = () => {
+    if (
+      selectedPage < Math.ceil(filteredAndSortedData.length / handlePageCount)
+    ) {
+      setSelectedPage(selectedPage + 1)
+    }
+  }
+  const onLast = () => {
+    setSelectedPage(Math.ceil(filteredAndSortedData.length / handlePageCount))
   }
 
   const handleViewClick = (value, e) => {
@@ -144,7 +221,7 @@ const Viewhistory: React.FC<IDefaultPageProps> = props => {
               }
             }}
           />
-          <p className="pagination-total-count">{`${selectedPage}-${handlePageCount} of 30`}</p>
+          <p className="pagination-total-count">{`${selectedPage}-${totalPages} of ${totalItems}`}</p>
         </div>
       </div>
     )
@@ -176,49 +253,85 @@ const Viewhistory: React.FC<IDefaultPageProps> = props => {
             <table className="table">
               <thead>
                 <tr className="head-style">
-                  <th scope="col">
+                  <th scope="col" onClick={() => sortHandle('name')}>
                     Name
                     <img
                       src={SortIcon}
-                      className="sort-icon"
-                      onClick={sortHandle}
+                      style={{ cursor: 'pointer' }}
+                      className={`sort-icon ${
+                        sortBy === 'name' ? 'active' : ''
+                      }`}
                       alt=""
                       aria-hidden="true"
                     />
                   </th>
-                  <th scope="col ">Date</th>
-                  <th scope="col">Time</th>
+                  <th scope="col" onClick={() => sortHandle('created_date')}>
+                    Date
+                    <img
+                      src={SortIcon}
+                      style={{ cursor: 'pointer' }}
+                      className={`sort-icon ${
+                        sortBy === 'name' ? 'active' : ''
+                      }`}
+                      alt=""
+                      aria-hidden="true"
+                    />
+                  </th>
+                  <th scope="col" onClick={() => sortHandle('created_time')}>
+                    Time
+                    <img
+                      src={SortIcon}
+                      style={{ cursor: 'pointer' }}
+                      className={`sort-icon ${
+                        sortBy === 'name' ? 'active' : ''
+                      }`}
+                      alt=""
+                      aria-hidden="true"
+                    />
+                  </th>
                   <th scope="col">Image</th>
-                  <th scope="col">Saved Variation</th>
+                  <th scope="col" onClick={() => sortHandle('variants')}>
+                    Saved Variation
+                    <img
+                      src={SortIcon}
+                      style={{ cursor: 'pointer' }}
+                      className={`sort-icon ${
+                        sortBy === 'name' ? 'active' : ''
+                      }`}
+                      alt=""
+                      aria-hidden="true"
+                    />
+                  </th>
                   <th scope="col">View</th>
                   <th scope="col">Action</th>
                 </tr>
               </thead>
               <tbody>
-                {data?.map((value, index) => (
-                  <tr key={index} className="body-style">
-                    <td>{value.name}</td>
-                    <td>{value.created_date}</td>
-                    <td>{value.created_time}</td>
-                    <td>
-                      <img
-                        className="vimage-style"
-                        src={value.original_url}
-                        alt="original image"
-                      />
-                    </td>
+                {!!currentDataPage &&
+                  currentDataPage?.map((value, index) => (
+                    <tr key={index} className="body-style">
+                      <td>{value.name}</td>
+                      <td>{value.created_date}</td>
+                      <td>{value.created_time}</td>
+                      <td>
+                        <img
+                          className="vimage-style"
+                          src={value.original_url}
+                          alt="original image"
+                        />
+                      </td>
 
-                    <td>{value.variants}</td>
-                    <td
-                      onClick={e => handleViewClick(value, e)}
-                      className="pointer"
-                    >
-                      <img className="eye-icon" src={icon} alt="eye image" />
-                    </td>
-                    <td>Action</td>
-                  </tr>
-                ))}
-                {!data && (
+                      <td>{value.variants}</td>
+                      <td
+                        onClick={e => handleViewClick(value, e)}
+                        className="pointer"
+                      >
+                        <img className="eye-icon" src={icon} alt="eye image" />
+                      </td>
+                      <td>Action</td>
+                    </tr>
+                  ))}
+                {currentDataPage?.length === 0 && (
                   <tr className="body-style">
                     <td colSpan={7} className="no-data-found">
                       No Data available
