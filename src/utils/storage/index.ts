9b00c48f05f3988b73850ea07c6c64f52ac758f2
sowri <sowrimalaiyan.m@gramenerit.com>
2023-12-27 11:01:25 +0530

    client_ui-fix-header-show-logged-username

--- src/utils/storage/index.ts
+++ src/utils/storage/index.ts
@@ -9,6 +9,15 @@ export const getAuthToken = (updateToken?: any) => {
     return localStorage.getItem(STORAGE_KEY.AUTH_TOKEN);
 };
 
+export const getLoggedUserName = (userData?:any) => {
+    if(userData) {
+        debugger;
+       localStorage.setItem(STORAGE_KEY.LOGGED_USER_NAME,userData?.data.name);
+       return userData?.data.name;
+    }
+    return  localStorage.getItem(STORAGE_KEY.LOGGED_USER_NAME);
+}
+
 export const IS_USER_AUTHENTICATED = (updateAuthenticated?: boolean) => {
     if (updateAuthenticated) {
         return localStorage.setItem('isUserAuthenticated', String(updateAuthenticated));
