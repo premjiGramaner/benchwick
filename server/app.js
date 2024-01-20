baac4d2d42ffdae67edd8ab212224d291249dfd8
premkumar.t <premkumar.t@thinkpalm.com>
2024-01-20 16:41:18 +0530

    server changes

--- server/app.js
+++ server/app.js
@@ -2,13 +2,11 @@ const express = require('express'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     path = require('path'),
-    sequlizer = require('./models/index'),
     dotenv = require('dotenv').config(),
     indexRouter = require('./routes/index'),
     app = express(),
     cors = require('cors'),
     fileUpload = require('express-fileupload');
-
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,22 +18,22 @@ app.use(cors());
 
 app.use('/api', indexRouter);
 
-
 app.use(express.static(path.join(__dirname, '../build')));
 app.use(express.static(path.join(__dirname, '../src')));
+
 
 app.use('/varients-images', express.static(__dirname + '/varients-images')); // giving public access
 app.use('/varients-generated', express.static(__dirname + '/varients-generated')); // giving public access
 
-
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.json({ error: err });
+app.use((req, res, next) => {
+    if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
+        next();
+    } else {
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        res.header('Expires', '-1');
+        res.header('Pragma', 'no-cache');
+        res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    }
 });
 
 app.get('/*', function (request, res) {
@@ -44,18 +42,6 @@ app.get('/*', function (request, res) {
             res.status(500).send(err)
         }
     })
-});
-
-/**
- * Initaing the Database Connection
- */
-sequlizer.sequelize.sync({ force: false }).then(() => {
-    // inside our db sync callback, we start the server
-    // this is our way of making sure the server is not listening 
-    // to requests if we have not made a db connection
-    app.listen(app.Sequlizer_port, () => {
-        console.log(`server listening on PORT ${app.Sequlizer_port}`);
-    });
 });
 
 module.exports = app;
